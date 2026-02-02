569c64cb76813a40251228ee528be8780afd1622
Vento <vento@tgsoft.ch>
2026-02-02 15:02:00 +0100

    WIP

--- index.php
+++ index.php
@@ -1,113 +1,130 @@
 <?php
-    if(isset($_GET['find_scanners'])) {
-        $pre = [];
-        $scanners = [];
-        $lines = [];
-        exec('avahi-browse -rt _uscan._tcp', $lines);
-        $expect = 'device';
-        $name = '';
-        $ip = '';
-        $port = '';
-        $proto = '';
-        $path = '';
-        foreach ($lines as $line) {
-            //$pre[] = "LINE: $line\n";
-            switch($expect) {
-                case 'device':
-                    if(preg_match('/^= \w+ IPv\d ([\w \-\[\]]+?)\s+_uscan/', $line, $matches)) {
-                        $name = $matches[1];
-                        $pre[] = "NAME: $name\n";
-                        $ip = '';
-                        $expect = 'ip';
-                    }
-                    break;
-                case 'ip':
-                    if(preg_match('/^\s+address = \[([\d\.:]+)\]+$/', $line, $matches)) {
-                        $ip = $matches[1];
-                        $pre[] = "IP: $ip\n";
-                        $port = '';
-                        $proto = '';
-                        $expect = 'port';
-                    }
-                    break;
-                case 'port':
-                    if(preg_match('/^\s+port = \[([\d\.]+)\]+$/', $line, $matches)) {
-                        $port = $matches[1];
-                        $proto = ($port == 443 || $port == 8082) ? 'https' : 'http';
-                        $pre[] = "PORT: $port\n";
-                        $pre[] = "PROTO: $proto\n";
-                        $path = '';
-                        $expect = 'path';
-                    }
-                    break;
-                case 'path':
-                    if(preg_match('/^\s+txt = .*"rs=(\w+?)"/', $line, $matches)) {
-                        $path = $matches[1];
-                        $pre[] = "PATH: $path\n";
+if (isset($_GET['find_scanners'])) {
+    $pre = [];
+    $scanners = [];
+    $lines = [];
+    exec('avahi-browse -rt _uscan._tcp', $lines);
+    $expect = 'device';
+    $name = '';
+    $ip = '';
+    $port = '';
+    $proto = '';
+    $path = '';
+    foreach ($lines as $line) {
+        //$pre[] = "LINE: $line\n";
+        switch ($expect) {
+            case 'device':
+                if (preg_match('/^= \w+ IPv\d ([\w \-\[\]]+?)\s+_uscan/', $line, $matches)) {
+                    $name = $matches[1];
+                    $pre[] = "NAME: $name\n";
+                    $ip = '';
+                    $expect = 'ip';
+                }
+                break;
+            case 'ip':
+                if (preg_match('/^\s+address = \[([\d\.:]+)\]+$/', $line, $matches)) {
+                    $ip = $matches[1];
+                    $pre[] = "IP: $ip\n";
+                    $port = '';
+                    $proto = '';
+                    $expect = 'port';
+                }
+                break;
+            case 'port':
+                if (preg_match('/^\s+port = \[([\d\.]+)\]+$/', $line, $matches)) {
+                    $port = $matches[1];
+                    $proto = ($port == 443 || $port == 8082) ? 'https' : 'http';
+                    $pre[] = "PORT: $port\n";
+                    $pre[] = "PROTO: $proto\n";
+                    $path = '';
+                    $expect = 'path';
+                }
+                break;
+            case 'path':
+                if (preg_match('/^\s+txt = .*"rs=(\w+?)"/', $line, $matches)) {
+                    $path = $matches[1];
+                    $pre[] = "PATH: $path\n";
 
-                        $scanners["$name [$ip]"] = "escl:$proto://$ip:$port/$path";
+                    $scanners["$name [$ip]"] = "escl:$proto://$ip:$port/$path";
 
-                        $device = '';
-                        $expect = 'device';
-                    }
-                    break;
-            }
+                    $device = '';
+                    $expect = 'device';
+                }
+                break;
         }
-        if(empty($scanners)) {
-            echo '<option value="" disabled selected>No scanners found, please search again...</option>' . "\n";
-        } else {
-            echo '<option value="" disabled selected>Please select...</option>' . "\n";
-            foreach ($scanners as $name => $scanpath) {
-                echo "<option value=\"$scanpath\">$name</option>\n";
-            }
-        }
-        echo '<option value="_search">Search for more scanners...</option>' . "\n";
-        exit;
     }
+    if (empty($scanners)) {
+        echo '<option value="" disabled selected>No scanners found, please search again...</option>' . "\n";
+    } else {
+        echo '<option value="" disabled selected>Please select...</option>' . "\n";
+        foreach ($scanners as $name => $scanpath) {
+            echo "<option value=\"$scanpath\">$name</option>\n";
+        }
+    }
+    echo '<option value="_search">Search for more scanners...</option>' . "\n";
+    exit;
+}
 
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <script src="jquery-4.0.0.min.js"></script>
-        <title>ScanTest</title>
-        <style>
-            * {
-                margin: 0;
-                padding: 0;
-            }
-            body {
-                width: 100vw;
-                height: 100vh;
-                padding: 25px;
-                font-family: 'Roboto', sans-serif;
-                font-size: 1.2em;
-                font-style: normal;
-            }
-            input, select {
-                font-size: 1em;
-            }
-            h2, p {
-                margin-bottom: 18px;
-            }
-            span {
-                color: darkgreen;
-            }
-        </style>
-    </head>
-    <body>
-        <h2>TG-Soft / graphax Linux Scanning Test Environment</h2>
-        <p>eSCL capable devices: <select name="scanner">
+
+<head>
+    <meta charset="UTF-8" />
+    <script src="jquery-4.0.0.min.js"></script>
+    <title>LinuxScanningLaboratory</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            padding: 25px;
+            font-family: 'Roboto', sans-serif;
+            font-size: 1.2em;
+            font-style: normal;
+        }
+
+        input,
+        select {
+            font-size: 1em;
+        }
+
+        h2,
+        p {
+            margin-bottom: 18px;
+        }
+
+        p>span {
+            color: darkgreen;
+        }
+    </style>
+</head>
+
+<body>
+    <span style="float:right;"><span id="version"></span><span id="upgrade"></span></span>
+    <h2>TG-Soft / graphax LinuxScanningLaboratory</h2>
+    <p>eSCL capable devices: <select name="scanner">
             <option value="_search" disabled selected>Searching, please wait...</option>
         </select>
-        <p>Active scanner: <span id="activeScanner"></span>
+    <p>Active scanner: <span id="activeScanner"></span>
         <!-- <pre><?= print_r($scanners) ?></pre> -->
         <script>
             let activeScanner = '';
             $(function() {
+                $.getJSON('release.json', function(localdata) {
+                    if (!localdata) return;
+                    $('#version').text('Version ' + localdata.version);
+                    $.getJSON('https://raw.githubusercontent.com/TG-Soft-GmbH/LinuxScanningLaboratory/refs/heads/main/release.json', function(remotedata) {
+                        if (!remotedata) return;
+                        if(localdata.version != remotedata.version) {
+                            $('#upgrade').html(` - <a href="/upgrade" style="color:darkorange;">Upgrade to Version ${remotedata.version}</a>`);
+                        }
+                    });
+                });
                 let $scannersel = $('select[name=scanner]');
                 $scannersel.on('change', function() {
-                    if($scannersel.val() == '_search') {
+                    if ($scannersel.val() == '_search') {
                         $scannersel.load('?find_scanners');
                         $('#activeScanner').text('');
                     } else {
@@ -117,5 +134,6 @@
                 $scannersel.load('?find_scanners');
             });
         </script>
-    </body>
+</body>
+
 </html>
