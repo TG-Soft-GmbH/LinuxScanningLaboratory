d06dbdec3e802b5c609c4d92ff1e91f4b8e53a68
VentoZwerg <box12@openspace.ch>
2026-02-06 14:19:01 +0100

    WIP

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 let activeScanner = '';
 $(function () {
-    function doUpgrade() {
-        $('.upgradeoverlay').show();
-        setTimeout(() => location.reload(true), 15000);
-        return true;
-    }
-
-    $.getJSON(`release.json?${ts}`, function (localdata) {
-        if (!localdata) return;
+    function manageVersion(localdata) {
         $('.version').text('Version ' + localdata.version);
         $.getJSON(`https://raw.githubusercontent.com/TG-Soft-GmbH/LinuxScanningLaboratory/refs/heads/main/release.json?${ts}`, function (remotedata) {
             if (!remotedata) return;
             if (localdata.version != remotedata.version) {
                 $('.upgrade').html(` - <a href="?upgrade">Upgrade to Version ${remotedata.version}</a>`);
-                $('.upgrade').find('a').on('click', doUpgrade);
+                $('.upgrade').find('a').on('click', function () {
+                    $('.upgradeoverlay').show();
+                    setTimeout(() => location.reload(true), 45000);
+                    return true;
+                });
             }
         });
-    });
-    
+    }
+    $.getJSON(`release.json?${ts}`).done(manageVersion).fail(manageVersion);
+
     let interval = null;
     window.addEventListener("message", function (e) {
         if (e.data === "execstream_done") {
@@ -28,7 +26,7 @@ $(function () {
     interval = setInterval(function () {
         $("#execresult").text($("#execiframe").contents().find("pre").text());
     }, 250);
-    
+
     let $scannersel = $('select[name=scanner]');
     $scannersel.on('change', function () {
         if ($scannersel.val() == '_search') {
